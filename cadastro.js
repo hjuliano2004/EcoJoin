eb2261407ba702bf4b86246d2e3f71c47066a153
hjuliano2004 <112650184+hjuliano2004@users.noreply.github.com>
2025-07-01 21:21:06 -0300

    estrutura do formulario concluida

---
+++ cadastro.js
@@ -0,0 +1,9 @@
+let enviar = document.getElementById("enviar");
+let form = document.getElementById("cadastraParceiro");
+
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    console.log("evento click")
+})
